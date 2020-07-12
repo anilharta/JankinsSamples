959f750e55e5f757618154a50519c93ea301bc90
anilharta <45859722+anilharta@users.noreply.github.com>
2020-07-12 14:42:31 +0530

    Add files via upload

---
+++ JankinsSamplesFirst.java
@@ -0,0 +1,10 @@
+package jankinsSamples;
+
+public class JankinsSamplesFirst {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Harta !!! This will be executed by Jenkins");
+
+	}
+
+}
